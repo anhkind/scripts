d6c85b57e239d9b62ae12d0639396313cfe4c562
Anh Nguyen <anhkind@gmail.com>
2018-12-22 13:53:35 +0800

    Update script for Ubuntu setup

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,3 +1,6 @@
+# add permission from vboxsf
+sudo adduser $USER vboxsf
+
 # ~/.ssh/authorized_keys
 export PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDj1Aq38rSXKulw0nDNbEAqmTkr8WZ+GTy/MgaSRe43gikKIjg+GTfBiP1ekWQwOhqYXaUhuTughzGeDyDZ/f3EHaElWg3xSi2YB5KeIL7FLNpMe9pAql4MHwxZTUoj4mE70L65rk90YZR/kHge+x5laZwfheg20w01XsiGIZXXr3VuiGDFr9omHMIJBNKYouFrGiWwklfPRTTJp61CA08LPpV2ROoBON0GBpMYo38IPcCOwGQFo6wZWYPf/Tun3ABQj5qQLPWoG6EAfJodiDuvH1Xh+dQYBOUKF0j1vz/C7ar8Y0JmmrPgIQl+OlJkpX1FSHgvSwul0IGHd9fmYKzX"
 touch ~/.ssh/authorized_keys && chmod 700 ~/.ssh/authorized_keys
