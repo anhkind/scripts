956b772dbf85acb05dedf529b7a824425a50baf5
Anh Nguyen <anhkind@gmail.com>
2018-12-22 17:05:46 +0800

    Update virtualbox script

--- ubuntu/virtualbox.sh
+++ ubuntu/virtualbox.sh
@@ -15,5 +15,6 @@ VBoxManage setextradata "$VM_NAME" VBoxInternal2/SharedFoldersEnableSymlinksCrea
 HOST_IP="127.0.0.1"
 GUEST_IP="10.0.2.15"
 VBoxManage modifyvm "$VM_NAME" --natpf1 "guestssh,tcp,$HOST_IP,2222,$GUEST_IP,22"
+VBoxManage modifyvm "$VM_NAME" --natpf1 "guestpostgres,tcp,$HOST_IP,5432,$GUEST_IP,5432"
 VBoxManage modifyvm "$VM_NAME" --natpf1 "guest3000,tcp,$HOST_IP,3000,$GUEST_IP,3000"
 VBoxManage modifyvm "$VM_NAME" --natpf1 "guest4200,tcp,$HOST_IP,4200,$GUEST_IP,4200"
