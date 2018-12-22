9a9f77509e8c1acf4ed58017ea1ae823e434b6a9
Anh Nguyen <anhkind@gmail.com>
2018-12-22 17:22:35 +0800

    Update ubuntu bootstrap script and virtualbox setup script

--- ubuntu/1_bootstrap.sh
+++ ubuntu/1_bootstrap.sh
@@ -1,7 +1,6 @@
 # add virtualbox guest additions
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq dkms build-essential linux-headers-generic linux-headers-$(uname -r) virtualbox-guest-additions-iso
 sudo mount /dev/cdrom /mnt
-sudo apt-get install -y dkms build-essential linux-headers-generic linux-headers-$(uname -r)
-
 sudo su -c '/mnt/VBoxLinuxAdditions.run'
 
 # add permission from vboxsf
@@ -11,3 +10,6 @@ sudo adduser $USER vboxsf
 export PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDj1Aq38rSXKulw0nDNbEAqmTkr8WZ+GTy/MgaSRe43gikKIjg+GTfBiP1ekWQwOhqYXaUhuTughzGeDyDZ/f3EHaElWg3xSi2YB5KeIL7FLNpMe9pAql4MHwxZTUoj4mE70L65rk90YZR/kHge+x5laZwfheg20w01XsiGIZXXr3VuiGDFr9omHMIJBNKYouFrGiWwklfPRTTJp61CA08LPpV2ROoBON0GBpMYo38IPcCOwGQFo6wZWYPf/Tun3ABQj5qQLPWoG6EAfJodiDuvH1Xh+dQYBOUKF0j1vz/C7ar8Y0JmmrPgIQl+OlJkpX1FSHgvSwul0IGHd9fmYKzX"
 touch ~/.ssh/authorized_keys && chmod 700 ~/.ssh/authorized_keys
 echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
+
+# create new key
+yes y |ssh-keygen -q -t rsa -N '' >/dev/null
--- virtualbox/setup.sh
+++ virtualbox/setup.sh
@@ -1,6 +1,20 @@
-# make symlink work on shared folder (from: https://github.com/npm/npm/issues/992#issuecomment-174154202)
 VM_NAME="Ubuntu"
 SHARE_NAME="dev"
+HOST_LOCAL_PATH="/Users/anhkind"
+SHARED_PATH="$HOST_LOCAL_PATH/$SHARE_NAME"
+
+# add shared folder
+VBoxManage sharedfolder add $VM_NAME --name $SHARE_NAME --hostpath $SHARED_PATH --automount
+
+# make symlink work on shared folder (from: https://github.com/npm/npm/issues/992#issuecomment-174154202)
 VBoxManage setextradata "$VM_NAME" VBoxInternal2/SharedFoldersEnableSymlinksCreate/$SHARE_NAME 1
 # check again
-VBoxManage getextradata "$VM_NAME" enumerate
+# VBoxManage getextradata "$VM_NAME" enumerate
+
+# add port forwarding
+HOST_IP="127.0.0.1"
+GUEST_IP="10.0.2.15"
+VBoxManage modifyvm "$VM_NAME" --natpf1 "guestssh,tcp,$HOST_IP,2222,$GUEST_IP,22"
+VBoxManage modifyvm "$VM_NAME" --natpf1 "guestpostgres,tcp,$HOST_IP,5432,$GUEST_IP,5432"
+VBoxManage modifyvm "$VM_NAME" --natpf1 "guest3000,tcp,$HOST_IP,3000,$GUEST_IP,3000"
+VBoxManage modifyvm "$VM_NAME" --natpf1 "guest4200,tcp,$HOST_IP,4200,$GUEST_IP,4200"
