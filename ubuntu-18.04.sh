be1fabb4d6b29989c67b606137f46a9325115632
Anh Nguyen <anhkind@gmail.com>
2018-12-22 13:20:46 +0800

    Update script for Ubuntu setup

--- ubuntu-18.04.sh
+++ ubuntu-18.04.sh
@@ -30,9 +30,8 @@ echo 'eval "$(rbenv init -)"' >> ~/.profile
 # ruby 2.3 & bundler
 export RUBY_VERSION=2.3.0
 sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install zlib1g-dev libssl1.0 libssl1.0-dev
-rbenv install $RUBY_VERSION
+rbenv install -s $RUBY_VERSION
 rbenv global $RUBY_VERSION
-gem install bundler
 
 # node 8
 brew install node@8
@@ -44,5 +43,5 @@ brew install yarn
 
 # ~/.ssh/authorized_keys
 export PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDj1Aq38rSXKulw0nDNbEAqmTkr8WZ+GTy/MgaSRe43gikKIjg+GTfBiP1ekWQwOhqYXaUhuTughzGeDyDZ/f3EHaElWg3xSi2YB5KeIL7FLNpMe9pAql4MHwxZTUoj4mE70L65rk90YZR/kHge+x5laZwfheg20w01XsiGIZXXr3VuiGDFr9omHMIJBNKYouFrGiWwklfPRTTJp61CA08LPpV2ROoBON0GBpMYo38IPcCOwGQFo6wZWYPf/Tun3ABQj5qQLPWoG6EAfJodiDuvH1Xh+dQYBOUKF0j1vz/C7ar8Y0JmmrPgIQl+OlJkpX1FSHgvSwul0IGHd9fmYKzX"
-touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys
+touch ~/.ssh/authorized_keys && chmod 700 ~/.ssh/authorized_keys
 echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
